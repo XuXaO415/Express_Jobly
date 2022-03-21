02432981e2baeae03b0b43ab352956eca48841ed
XuXaO415 <52443523+XuXaO415@users.noreply.github>
2022-03-20 21:12:44 -0700

    Added documentation and explanation to this section

--- express-jobly/helpers/sql.js
+++ express-jobly/helpers/sql.js
@@ -3,18 +3,20 @@ const { BadRequestError } = require("../expressError");
 // THIS NEEDS SOME GREAT DOCUMENTATION.
 
 function sqlForPartialUpdate(dataToUpdate, jsToSql) {
-  const keys = Object.keys(dataToUpdate);
-  if (keys.length === 0) throw new BadRequestError("No data");
+    //enumerates through updatable data objects/properties in db i.e company, jobs
+    const keys = Object.keys(dataToUpdate);
+    // if keys are empty, throw error that states no data (ex: applications in db is empty, this would throw an error)
+    if (keys.length === 0) throw new BadRequestError("No data");
 
-  // {firstName: 'Aliya', age: 32} => ['"first_name"=$1', '"age"=$2']
-  const cols = keys.map((colName, idx) =>
-      `"${jsToSql[colName] || colName}"=$${idx + 1}`,
-  );
+    // {firstName: 'Aliya', age: 32} => ['"first_name"=$1', '"age"=$2']
+    const cols = keys.map((colName, idx) =>
+        `"${jsToSql[colName] || colName}"=$${idx + 1}`,
+    );
 
-  return {
-    setCols: cols.join(", "),
-    values: Object.values(dataToUpdate),
-  };
+    return {
+        setCols: cols.join(", "),
+        values: Object.values(dataToUpdate),
+    };
 }
 
 module.exports = { sqlForPartialUpdate };
