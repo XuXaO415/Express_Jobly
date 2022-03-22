86b2735fcbf97ed46c405d2b3c1a72f574e3066f
XuXaO415 <52443523+XuXaO415@users.noreply.github>
2022-03-21 19:07:30 -0700

    Jest test error. Will need to add filtering criteria to models

--- express-jobly/routes/companies.js
+++ express-jobly/routes/companies.js
@@ -11,6 +11,7 @@ const Company = require("../models/company");
 
 const companyNewSchema = require("../schemas/companyNew.json");
 const companyUpdateSchema = require("../schemas/companyUpdate.json");
+const db = require("../db");
 
 const router = new express.Router();
 
@@ -24,19 +25,19 @@ const router = new express.Router();
  * Authorization required: login
  */
 
-router.post("/", ensureLoggedIn, async function (req, res, next) {
-  try {
-    const validator = jsonschema.validate(req.body, companyNewSchema);
-    if (!validator.valid) {
-      const errs = validator.errors.map(e => e.stack);
-      throw new BadRequestError(errs);
-    }
+router.post("/", ensureLoggedIn, async function(req, res, next) {
+    try {
+        const validator = jsonschema.validate(req.body, companyNewSchema);
+        if (!validator.valid) {
+            const errs = validator.errors.map(e => e.stack);
+            throw new BadRequestError(errs);
+        }
 
-    const company = await Company.create(req.body);
-    return res.status(201).json({ company });
-  } catch (err) {
-    return next(err);
-  }
+        const company = await Company.create(req.body);
+        return res.status(201).json({ company });
+    } catch (err) {
+        return next(err);
+    }
 });
 
 /** GET /  =>
@@ -48,15 +49,23 @@ router.post("/", ensureLoggedIn, async function (req, res, next) {
  * - nameLike (will find case-insensitive, partial matches)
  *
  * Authorization required: none
+ * 
+ * ** ADD a feature that allows a user to filter the results based on optional filtering criteria...look at lines 45-48 **
  */
 
-router.get("/", async function (req, res, next) {
-  try {
-    const companies = await Company.findAll();
-    return res.json({ companies });
-  } catch (err) {
-    return next(err);
-  }
+router.get("/", async function(req, res, next) {
+    try {
+        let companies;
+        const { minEmployees, maxEmployees, nameLike } = req.query;
+        if (!minEmployees && maxEmployees && !nameLike) {
+            companies = await Company.findAll();
+        } else {
+            companies = await Company.update(req.query);
+        }
+        return res.json({ companies });
+    } catch (err) {
+        return next(err);
+    }
 });
 
 /** GET /[handle]  =>  { company }
@@ -67,13 +76,13 @@ router.get("/", async function (req, res, next) {
  * Authorization required: none
  */
 
-router.get("/:handle", async function (req, res, next) {
-  try {
-    const company = await Company.get(req.params.handle);
-    return res.json({ company });
-  } catch (err) {
-    return next(err);
-  }
+router.get("/:handle", async function(req, res, next) {
+    try {
+        const company = await Company.get(req.params.handle);
+        return res.json({ company });
+    } catch (err) {
+        return next(err);
+    }
 });
 
 /** PATCH /[handle] { fld1, fld2, ... } => { company }
@@ -87,19 +96,19 @@ router.get("/:handle", async function (req, res, next) {
  * Authorization required: login
  */
 
-router.patch("/:handle", ensureLoggedIn, async function (req, res, next) {
-  try {
-    const validator = jsonschema.validate(req.body, companyUpdateSchema);
-    if (!validator.valid) {
-      const errs = validator.errors.map(e => e.stack);
-      throw new BadRequestError(errs);
-    }
+router.patch("/:handle", ensureLoggedIn, async function(req, res, next) {
+    try {
+        const validator = jsonschema.validate(req.body, companyUpdateSchema);
+        if (!validator.valid) {
+            const errs = validator.errors.map(e => e.stack);
+            throw new BadRequestError(errs);
+        }
 
-    const company = await Company.update(req.params.handle, req.body);
-    return res.json({ company });
-  } catch (err) {
-    return next(err);
-  }
+        const company = await Company.update(req.params.handle, req.body);
+        return res.json({ company });
+    } catch (err) {
+        return next(err);
+    }
 });
 
 /** DELETE /[handle]  =>  { deleted: handle }
@@ -107,13 +116,13 @@ router.patch("/:handle", ensureLoggedIn, async function (req, res, next) {
  * Authorization: login
  */
 
-router.delete("/:handle", ensureLoggedIn, async function (req, res, next) {
-  try {
-    await Company.remove(req.params.handle);
-    return res.json({ deleted: req.params.handle });
-  } catch (err) {
-    return next(err);
-  }
+router.delete("/:handle", ensureLoggedIn, async function(req, res, next) {
+    try {
+        await Company.remove(req.params.handle);
+        return res.json({ deleted: req.params.handle });
+    } catch (err) {
+        return next(err);
+    }
 });
 
 
