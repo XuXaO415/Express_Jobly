1af5e3bf1480135d5c6f985c6ed141851b98e66b
XuXaO415 <52443523+XuXaO415@users.noreply.github>
2022-04-08 21:33:19 -0700

    Added tests to this section

--- express-jobly/models/job.test.js
+++ express-jobly/models/job.test.js
@@ -18,6 +18,59 @@ beforeEach(commonBeforeEach);
 afterEach(commonAfterEach);
 afterAll(commonAfterAll);
 
+/************************************** create
+ * 
+ * Modeled after models/company.test.js
+ */
+describe("create", function() {
+    const newJob = {
+        companyHandle: "",
+        title: "",
+        salary: 100,
+        equity: "",
+    };
+    test("works", async function() {
+        let job = await Job.create(newJob);
+        expect(job).toEqual({
+            newJob: [{
+                    //from solutions
+                    id: expect.any(Number),
+                    title: "J1",
+                    salary: 1,
+                    equity: "3.14",
+                    companyHandle: "c1",
+                    companyHandle: "C1",
+                },
+                {
+                    id: expect.any(Number),
+                    title: "J2",
+                    salary: 2,
+                    equity: "0.2",
+                    companyHandle: "c1",
+                    companyName: "C1",
+                },
+                {
+                    id: expect.any(Number),
+                    title: "J3",
+                    salary: 3,
+                    equity: null,
+                    companyHandle: "c1",
+                    companyName: "C1",
+                },
+            ],
+        });
+    })
+});
+
+/************************************** findAll */
+
+describe("findAll", function() {
+    test("works: no filter", async function() {
+        let jobs = await Job.findAll();
+        expect(jobs).toEqual([{}])
+    });
+});
+
 /************************************** get */
 
 describe("GET", function() {
@@ -43,6 +96,28 @@ describe("GET", function() {
             fail();
         } catch (err) {
             expect(err instanceof NotFoundError).toBeTruthy();
+        }
+    });
+});
+
+/************************************** update */
+
+/************************************** remove */
+
+describe("remove", function() {
+    test("works", async function() {
+        await Job.remove(testJobIds[0]);
+        const res = await db.query(
+            `SELECT id FROM jobs WHERE id = $1`, [testJobIds[0]]
+        );
+        expect(res.rows.length).toEqual(0);
+    })
+    test("not found if no such job", async function() {
+        try {
+            await Job.remove(0);
+            fail();
+        } catch (err) {
+            expect(err instanceof NotFoundError).toBeTruth();
         }
     });
 });
